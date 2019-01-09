a56368c9ef8adaeb6d681da0905835bc6861c34f
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-01-09 11:15:24 +0100

    ReservationWindow deatiled

--- SD3_DeustoAir_Client/src/es/deusto/deustoair/client/gui/ReservationWindow.java
+++ SD3_DeustoAir_Client/src/es/deusto/deustoair/client/gui/ReservationWindow.java
@@ -11,12 +11,27 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class ReservationWindow {
 	private Object flight;
 	private JFrame frame;
 	private JPanel OptPanelC;
 	private JPanel OptPanelP;
+	private JLabel lblEmail;
+	private JLabel lblPassword;
+	private JButton btnPay;
+	private JTextField tfEmail;
+	private JTextField tfpass;
+	private JLabel label;
+	private JLabel lblCvv;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblExpiry;
+	private JTextField textField_2;
+	private JLabel lblSeats;
+	private JTextField textField_3;
 
 	public ReservationWindow(Object object) {
 		this.flight = object;
@@ -30,7 +45,7 @@ public class ReservationWindow {
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.setTitle(flight.toString());
-		frame.setBounds(100, 100, 500, 550);
+		frame.setBounds(100, 100, 500, 352);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -75,17 +90,77 @@ public class ReservationWindow {
 		lblPaymentMethod.setBounds(21, 13, 152, 39);
 		frame.getContentPane().add(lblPaymentMethod);
 		
-		OptPanelP = new JPanel();
-		OptPanelP.setBounds(21, 95, 450, 360);
-		OptPanelP.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "PayPal Info"));
-		OptPanelP.setVisible(false);
-		frame.getContentPane().add(OptPanelP);
-		
 		OptPanelC = new JPanel();
-		OptPanelC.setBounds(21, 95, 450, 360);
+		OptPanelC.setBounds(21, 95, 450, 166);
 		OptPanelC.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Credit Info"));
 		OptPanelC.setVisible(false);
 		frame.getContentPane().add(OptPanelC);
+		OptPanelC.setLayout(null);
+		
+		label = new JLabel("Email:");
+		label.setBounds(12, 48, 56, 16);
+		OptPanelC.add(label);
+		
+		lblCvv = new JLabel("CVV:");
+		lblCvv.setBounds(12, 75, 70, 16);
+		OptPanelC.add(lblCvv);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 43, 116, 22);
+		OptPanelC.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(80, 73, 116, 22);
+		OptPanelC.add(textField_1);
+		textField_1.setColumns(10);
+		
+		lblExpiry = new JLabel("Expiry:");
+		lblExpiry.setBounds(12, 104, 56, 16);
+		OptPanelC.add(lblExpiry);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(80, 101, 116, 22);
+		OptPanelC.add(textField_2);
+		textField_2.setColumns(10);
+		
+		btnPay = new JButton("Pay");
+		btnPay.setBounds(362, 274, 97, 25);
+		frame.getContentPane().add(btnPay);
+		
+		OptPanelP = new JPanel();
+		OptPanelP.setBounds(21, 95, 450, 166);
+		frame.getContentPane().add(OptPanelP);
+		OptPanelP.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "PayPal Info"));
+		OptPanelP.setVisible(false);
+		OptPanelP.setLayout(null);
+		
+		lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(12, 38, 56, 16);
+		OptPanelP.add(lblEmail);
+		
+		lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(12, 71, 70, 16);
+		OptPanelP.add(lblPassword);
+		
+		tfEmail = new JTextField();
+		tfEmail.setBounds(89, 35, 116, 22);
+		OptPanelP.add(tfEmail);
+		tfEmail.setColumns(10);
+		
+		tfpass = new JTextField();
+		tfpass.setBounds(89, 68, 116, 22);
+		OptPanelP.add(tfpass);
+		tfpass.setColumns(10);
+		
+		lblSeats = new JLabel("Seats Amount:");
+		lblSeats.setBounds(21, 274, 87, 16);
+		frame.getContentPane().add(lblSeats);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(120, 274, 39, 22);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
 		
 		frame.setVisible(true);
 	}
