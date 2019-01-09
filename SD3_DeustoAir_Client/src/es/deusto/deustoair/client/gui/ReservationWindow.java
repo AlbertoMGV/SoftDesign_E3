756eb74237e14c0ce112ec5f152fe71db293918f
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-01-09 10:57:10 +0100

    payment panels

--- SD3_DeustoAir_Client/src/es/deusto/deustoair/client/gui/ReservationWindow.java
+++ SD3_DeustoAir_Client/src/es/deusto/deustoair/client/gui/ReservationWindow.java
@@ -8,10 +8,15 @@ import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 
 public class ReservationWindow {
 	private Object flight;
 	private JFrame frame;
+	private JPanel OptPanelC;
+	private JPanel OptPanelP;
 
 	public ReservationWindow(Object object) {
 		this.flight = object;
@@ -33,16 +38,55 @@ public class ReservationWindow {
 		chckbxPayPal.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		chckbxPayPal.setBounds(21, 61, 113, 25);
 		frame.getContentPane().add(chckbxPayPal);
+		chckbxPayPal.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (chckbxPayPal.isSelected()) {
+					System.out.println("Paypal");
+					OptPanelC.setVisible(false);
+					OptPanelP.setVisible(true);
+					
+					
+				}
+				
+			}
+		});
 		
 		JCheckBox chckbxCredit = new JCheckBox("Credit");
 		chckbxCredit.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		chckbxCredit.setBounds(174, 61, 113, 25);
 		frame.getContentPane().add(chckbxCredit);
+		chckbxCredit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (chckbxCredit.isSelected()) {
+					System.out.println("Credit");
+					OptPanelP.setVisible(false);
+					OptPanelC.setVisible(true);
+				}
+				
+			}
+		});
 		
 		JLabel lblPaymentMethod = new JLabel("Payment Method");
 		lblPaymentMethod.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblPaymentMethod.setBounds(21, 13, 152, 39);
 		frame.getContentPane().add(lblPaymentMethod);
+		
+		OptPanelP = new JPanel();
+		OptPanelP.setBounds(21, 95, 450, 360);
+		OptPanelP.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "PayPal Info"));
+		OptPanelP.setVisible(false);
+		frame.getContentPane().add(OptPanelP);
+		
+		OptPanelC = new JPanel();
+		OptPanelC.setBounds(21, 95, 450, 360);
+		OptPanelC.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Credit Info"));
+		OptPanelC.setVisible(false);
+		frame.getContentPane().add(OptPanelC);
+		
 		frame.setVisible(true);
 	}
 }
