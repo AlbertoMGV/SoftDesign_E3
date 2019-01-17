60f4650acc1a9f069f251ca40a45bbfa6e5a4264
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-01-17 16:17:25 +0100

    added method

--- SD3_DeustoAir_Server/src/es/deusto/deustoair/server/remote/IDeustoAirServerRemote.java
+++ SD3_DeustoAir_Server/src/es/deusto/deustoair/server/remote/IDeustoAirServerRemote.java
@@ -9,10 +9,17 @@ import es.deusto.deustoair.server.data.dto.PaymentDTO;
 import es.deusto.deustoair.server.data.dto.ReservationDTO;
 import es.deusto.deustoair.server.data.dto.UserDTO;
 
+import es.deusto.deustoair.server.data.Flight;
+import es.deusto.deustoair.server.data.User;
+
 public interface IDeustoAirServerRemote extends Remote {
 	public boolean register(String username, String password, String authMethod, AirportDTO defaultAirport) throws RemoteException;
 	public boolean login(String username, String password, String authMethod) throws RemoteException;
 	public FlightDTO[] searchFlights(AirportDTO origin, AirportDTO destination, String dDate, String rDate, int seats) throws RemoteException;	
 	public boolean orderFlight(FlightDTO flight, int seats, UserDTO user) throws RemoteException;
 	public PaymentDTO pay(ReservationDTO res, String args, String type) throws RemoteException;
+
+	public User getUser(String emailUser) throws RemoteException;
+	public Flight getFlight(String codeFlight) throws RemoteException;
+	public void storeReservation(User usr) throws RemoteException;
 }
